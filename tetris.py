51dacd1cc29e7fd4b1b8922d7862f51bc2feede5
JericHunter <jeric.hunterr@gmail.com>
2020-10-15 05:12:05 -0400

    adding global variables

---
+++ tetris.py
@@ -0,0 +1,14 @@
+import pygame
+import random
+
+
+pygame.font.init()
+
+screen_width = 800
+screen_height = 700
+game_width = 300 
+game_height = 600
+block_size = 30
+
+top_left_x = (screen_width - game_width) // 2
+top_left_y = screen_height - game_height
