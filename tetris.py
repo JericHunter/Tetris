f68c0ae8796396f3207bee997b3feb74945358eb
JericHunter <jeric.hunterr@gmail.com>
2020-10-16 01:02:18 -0400

    valid space

--- tetris.py
+++ tetris.py
@@ -156,3 +156,15 @@ def convertShapeFormat(shape):
         blockPositions[i] = (pos[0] - 2, pos[1] - 4)
 
     return blockPositions
+
+def validSpace(shape, grid):
+    acceptedPositions = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    acceptedPositions = [j for sub in acceptedPositions for j in sub]
+    formatted = convertShapeFormat(shape)
+
+    for pos in formatted:
+        if pos not in acceptedPositions:
+            if pos[1] > -1:
+                return False
+
+    return True
