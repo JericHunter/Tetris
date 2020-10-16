f0d8cf4a4cd3d87a6f8e80c6ca37479d3bf905c0
JericHunter <jeric.hunterr@gmail.com>
2020-10-16 01:19:58 -0400

    drawing grid with pygame

--- tetris.py
+++ tetris.py
@@ -188,3 +188,25 @@ def drawGrid(surface, row, col):
         pygame.draw.line(surface, (128,128,128), (sx, sy+ i*30), (sx + game_width, sy + i * 30))
         for j in range(col):
             pygame.draw.line(surface, (128,128,128), (sx + j * 30, sy), (sx + j * 30, sy + game_height))
+
+def clearRows(grid, locked):
+    # need to see if row is clear the shift every other row above down one
+
+    inc = 0
+    for i in range(len(grid)-1,-1,-1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            # add positions to remove from locked
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
